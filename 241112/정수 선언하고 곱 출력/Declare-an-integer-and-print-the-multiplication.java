f796eff2019f91f8502a0cb3262fc13d8d301b21
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-11-12 23:29:43 +0900

    Title: 정수 선언하고 곱 출력, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241112/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 26;
+        int num2 = 5;
+        int mul = num1 * num2;
+        System.out.print(num1+" * "+num2+" = "+mul);
+    }
+}

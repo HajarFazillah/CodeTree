d53c7fcaaab592b6c64d70f737d36fdb192db284
Hajar Fazillah <60101702+HajarFazillah@users.noreply.github.com>
2024-11-13 18:21:54 +0900

    Title: 변수 출력하기 3, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241113/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+        char c = 'C';
+        String mid = "->";
+        System.out.print(a+mid+b+mid+c);
+    }
+}
